15f4756f0bdcf635722808de86fe9ee5bd9d2b63
Lyon Suarez <lyoncs2000@gmail.com>
2023-04-22 19:10:59 -0300

    Actualizacion de avance

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+// Settings
+
+// Middlewares - Funciones que se ejecutan antes de que llegen a las rutas
+
+// Routes
+
+// Static files - Donde van los archivos estaticos los html los css lo javascrip
+
+//Starting the server
+
+
+app.listen(3000, () => {
+    console.log('server on port 3000');
+});
